bdb2bdf4782699335674ab741ab038c2d46ffdf0
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-11 01:29:56 -0300

    Starting Feed

---
+++ nft-shop/src/components/Feed/index.tsx
@@ -0,0 +1,22 @@
+import { SearchInput } from "../SearchInput";
+import { RiUploadCloud2Fill } from "react-icons/ri";
+import { BsFillChatFill } from "react-icons/bs";
+import styles from "./styles.module.scss";
+
+export const Feed = () => {
+  return (
+    <div className={styles.feedContainer}>
+      <div className={styles.topActionsBar}>
+        <SearchInput />
+        <div className={styles.topActionsButtons}>
+          <button>
+            <RiUploadCloud2Fill size={22} />
+          </button>
+          <button>
+            <BsFillChatFill size={22} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
