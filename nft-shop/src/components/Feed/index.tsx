634cd2f8eb89d3d67adb349e8a340573f14d776a
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-11 22:06:12 -0300

    Update index.tsx

--- nft-shop/src/components/Feed/index.tsx
+++ nft-shop/src/components/Feed/index.tsx
@@ -1,21 +1,18 @@
-import { SearchInput } from "../SearchInput";
-import { RiUploadCloud2Fill } from "react-icons/ri";
-import { BsFillChatFill } from "react-icons/bs";
 import styles from "./styles.module.scss";
+import { NFTBanner } from "../NFTBanner";
+import { TopActionsBar } from "../TopActionsBar";
+import { FeedActionsBar } from "../FeedActionsBar";
+import { NFTFeed } from "../NFTFeed";
 
 export const Feed = () => {
   return (
-    <div className={styles.feedContainer}>
-      <div className={styles.topActionsBar}>
-        <SearchInput />
-        <div className={styles.topActionsButtons}>
-          <button>
-            <RiUploadCloud2Fill size={22} />
-          </button>
-          <button>
-            <BsFillChatFill size={22} />
-          </button>
-        </div>
+    <div className={styles.feedPageContainer}>
+      <TopActionsBar />
+      <NFTBanner />
+
+      <div className={styles.feedContainer}>
+        <FeedActionsBar />
+        <NFTFeed />
       </div>
     </div>
   );
