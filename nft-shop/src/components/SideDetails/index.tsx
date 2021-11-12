b54a8f0d49778ef40f0a3ff42b90a04e94d21b6b
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-11 22:06:36 -0300

    Update index.tsx

--- nft-shop/src/components/SideDetails/index.tsx
+++ nft-shop/src/components/SideDetails/index.tsx
@@ -1,3 +1,12 @@
+import { NotificationsPanel } from "../NotificationsPanel";
+import { TopArtists } from "../TopArtists";
+import styles from "./styles.module.scss";
+
 export const SideDetails = () => {
-  return <></>;
+  return (
+    <div className={styles.sideDetailsContainer}>
+      <NotificationsPanel />
+      <TopArtists />
+    </div>
+  );
 };
