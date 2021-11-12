adf8d166cadc36255fda3acfc4a00dc66a891405
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-11 22:05:49 -0300

    Create nft-mocked-list.js

---
+++ nft-shop/src/nft-mocked-list.js
@@ -0,0 +1,54 @@
+import art1 from "./assets/art-1.jpg";
+import art2 from "./assets/art-2.jpg";
+import art3 from "./assets/art-3.jpg";
+
+export const nftMockedList = [
+  {
+    id: "1",
+    title: "NFT #0001",
+    price: "1.2 ETH",
+    biding: "24 people biding",
+    image: art1,
+    timeLeft: "1d 12 min left",
+  },
+  {
+    id: "2",
+    title: "NFT #0002",
+    price: "1 ETH",
+    biding: "12 people biding",
+    image: art2,
+    timeLeft: "1d 12 min left",
+  },
+  {
+    id: "3",
+    title: "NFT #0003",
+    price: "2 ETH",
+    biding: "3 people biding",
+    image: art3,
+    timeLeft: "1d 12 min left",
+  },
+  {
+    id: "4",
+    title: "NFT #0004",
+    price: "1.2 ETH",
+    biding: "24 people biding",
+    image: art1,
+    timeLeft: "1d 12 min left",
+  },
+  {
+    id: "5",
+    title: "NFT #0005",
+    price: "1 ETH",
+    biding: "12 people biding",
+    image: art2,
+    timeLeft: "1d 12 min left",
+  },
+  {
+    id: "6",
+    title: "NFT #0006",
+    price: "2 ETH",
+    biding: "3 people biding",
+    image: art3,
+    timeLeft: "1d 12 min left",
+  },
+];
